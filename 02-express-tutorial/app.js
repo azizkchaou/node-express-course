07dcb119c29530552ee4a9bda2d7b4c552b58631
aziz kchaou <mohamedaziz.kchaou@insat.ucar.tn>
2025-12-12 20:05:00 +0100

    a server with just node

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,1 +1,28 @@
-console.log('Express Tutorial')
+const http = require('http');
+const {readFileSync, read} = require('fs');
+
+// get all files 
+const homePage = readFileSync('./index.html');
+const server = http.createServer((req, res) => {
+    if (req.url === '/') {
+        console.log(req.method);
+        res.writeHead(200 , {'content-type' : 'text/html'});
+        res.write(homePage);
+        res.end();
+    }
+    else if (req.url ==='/about') {
+        console.log(req.url);
+        res.writeHead(200 , {'content-type' : 'text/html'});
+        res.end('<h1>here is our short history</h1>');
+    }
+    else {
+        res.writeHead(404 , {'content-type' : 'text/html'});
+        res.write(`
+        <h1>Ooops!</h1>
+        <p>we can't seem to find the page you are looking for</p>   
+        <a href="/">back home</a>
+        `);
+        res.end();
+    }
+});
+server.listen(5000);
