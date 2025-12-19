39ced757fa239520d2a8c085a7cd77b8aa568cb0
aziz kchaou <mohamedaziz.kchaou@insat.ucar.tn>
2025-12-19 12:24:30 +0100

    implement PUT and DELETE endpoints for managing people

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -39,6 +39,31 @@ app.post('/api/people/postman' , (req, res) => {
     res.status(201).json({success:true , data:[...people , name]});
 })
 
+app.put('/api/people/:id' , (req , res) => {
+    const {id} = req.params ;
+    const {name} = req.body ;
+    const person = people.find((person) => person.id === Number(id));
+    if (!person) {
+        return res.status(404).json({success:false , msg:`No person with id ${id}`});
+    }
+    const newPeople = people.map((person) => {
+        if (person.id === Number(id)) {
+            person.name = name ;
+        }
+        return person ;
+    });
+    res.status(200).json({success:true , data:newPeople});
+});
+app.delete('/api/people/:id' , (req , res) => {
+    const {id} = req.params ;
+    const person = people.find((person) => person.id === Number(id));
+    if (!person) {
+        return res.status(404).json({success:false , msg:`No person with id ${id}`});
+    }
+    const newPeople = people.filter((person) => person.id !== Number(id));
+    return res.status(200).json({success:true , data:newPeople});
+});
+
 app.listen(5000 , () => {
     console.log('Server is listening on port 5000...');
 })
